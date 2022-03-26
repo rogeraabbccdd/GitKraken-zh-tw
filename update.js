9efe22b10b8aec4fb47ef1de75d21cfa7fa1714a
we684123 <we684123@gmail.com>
2022-03-26 18:04:20 +0800

    ✨🍻 update.js 現在會自己去找要去帶的檔案(應該ㄅ

--- update.js
+++ update.js
@@ -1,5 +1,8 @@
+const os = require('os')
 const fs = require('fs')
 const path = require('path');
+const child_process = require('child_process');
+
 const rp = require('request-promise')
 
 const config = require('./config.json')
@@ -40,6 +43,74 @@ async function getRemoteStringsJSON_en(config) {
     config, config['remote_strings_en_name'])
 }
 
+async function replace_local_strings(newStringsJSON) {
+  // 判斷不同平台並設立正確的 strings_path
+  const platform_type = os.type()
+  let strings_path
+  console.log(`platform_type is ${platform_type}`);
+  if (platform_type == 'Linux') {
+    // 在 Linux 上
+    strings_path = "/usr/share/gitkraken/resources/app.asar.unpacked/src"
+    fs.access(strings_path, fs.constants.F_OK, (err) => {
+      console.log(`${strings_path} ${err ? '不存在' : '存在'}`);
+      if (err) {
+        // 不正確噴錯
+        logger.error(err)
+      } else {
+        // 取代 loacl 的語言檔
+        fs.writeFile('./strings.json', newStringsJSON, 'utf8', () => {
+          console.log('finished')
+        })
+      }
+    });
+  } else if (platform_type == 'Darwin') {
+    // 在 Darwin(macOS) 上
+    strings_path = [
+      "/Applications/GitKraken.app/Contents/",
+      "Resources/app.asar.unpacked/src/strings.json"
+    ].join('')
+    fs.access(strings_path, fs.constants.F_OK, (err) => {
+      console.log(`${strings_path} ${err ? '不存在' : '存在'}`);
+      if (err) {
+        // 不正確噴錯
+        logger.error(err)
+      } else {
+        // 取代 loacl 的語言檔
+        fs.writeFile('./strings.json', newStringsJSON, 'utf8', () => {
+          console.log('finished')
+        })
+      }
+    });
+  } else {
+    // 在 Windows_NT 上
+    // 生出 strings_path
+    await child_process.exec('gitkraken -v', (error, stdout, stderr) => {
+      let gitkraken_version = stdout
+      strings_path = [
+        process.env['LOCALAPPDATA'],
+        "\\gitkraken\\app-",
+        gitkraken_version,
+        `\\resources\\app.asar.unpacked\\src\\strings.json`,
+      ].join('').replace('\n', '')
+      console.log(`strings_path:\n${strings_path}`);
+
+      // 檢查生出來的 path 是否正確
+      fs.access(strings_path, fs.constants.F_OK, (err) => {
+        console.log(`${strings_path} ${err ? '不存在' : '存在'}`);
+        if (err) {
+          // 不正確噴錯
+          logger.error(err)
+        } else {
+          // 取代 loacl 的語言檔
+          fs.writeFile('./strings.json', newStringsJSON, 'utf8', () => {
+            console.log('finished')
+          })
+        }
+      });
+    })
+  }
+}
+
 async function main() {
   // 抓遠端語言 JSON 下來
   let values = await Promise.all([
@@ -51,11 +122,7 @@ async function main() {
 
   // 進行取代後輸出新的 JSON 語言檔
   const newStringsJSON = merge(enJSON, twJSON)
-
-  // 取代 loacl 的語言檔
-  fs.writeFile('./strings.json', newStringsJSON, 'utf8', () => {
-    console.log('finished')
-  })
+  replace_local_strings(newStringsJSON)
 }
 
 main()
