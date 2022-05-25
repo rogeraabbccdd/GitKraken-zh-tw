f6a6b2ca9264f21b45118d2e82c944218eecb487
we684123 <we684123@gmail.com>
2022-05-26 00:04:37 +0800

    ♻️🚨💄  update.js 修改同步的部份，比以前好看太多 , 修改 Linter 成 Perttier, 修改輸出畫面

--- update.js
+++ update.js
@@ -1,14 +1,13 @@
-const os = require('os')
-const fs = require('fs')
-const path = require('path');
-const child_process = require('child_process');
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
+const child_process = require("child_process");
 
-const rp = require('request-promise')
-const JSON5 = require('json5');
-require('json5/lib/register')
+const rp = require("request-promise");
+const JSON5 = require("json5");
+require("json5/lib/register");
 
-const config = require('./config.json5')
-
+const config = require("./config.json5");
 
 /**
  * 單純合併兩個 JSON 語言檔
@@ -20,11 +19,11 @@ function merge(enJSON, twJSON) {
   for (let key1st in enJSON) {
     for (let key2nd in enJSON[key1st]) {
       if (twJSON[key1st][key2nd]) {
-        enJSON[key1st][key2nd] = twJSON[key1st][key2nd]
+        enJSON[key1st][key2nd] = twJSON[key1st][key2nd];
       }
     }
   }
-  return JSON.stringify(enJSON, null, 2)
+  return JSON.stringify(enJSON, null, 2);
 }
 
 /**
@@ -35,17 +34,17 @@ function merge(enJSON, twJSON) {
  */
 async function getRemoteStringsJSON_by_language(config, remote_strings_url) {
   // 先從 ./update_version.json 中拿到設定並組出正確獲取翻譯 json 的 url
-  const rpl = new URL(config['remote_repo_url'])
+  const rpl = new URL(config["remote_repo_url"]);
   const remote_strings_tw_url = [
-    `${config['usercontent_base_url']}${rpl['pathname']}/`,
-    `${config['aims_branche']}/${remote_strings_url}`,
-  ].join('')
+    `${config["usercontent_base_url"]}${rpl["pathname"]}/`,
+    `${config["aims_branche"]}/${remote_strings_url}`
+  ].join("");
 
   // 用上面種里的 url 發出請求以獲得結果
   return await rp({
     url: remote_strings_tw_url,
-    json: true,
-  })
+    json: true
+  });
 }
 
 /**
@@ -55,7 +54,9 @@ async function getRemoteStringsJSON_by_language(config, remote_strings_url) {
  */
 async function getRemoteStringsJSON_tw(config) {
   return await getRemoteStringsJSON_by_language(
-    config, config['remote_strings_tw_name'])
+    config,
+    config["remote_strings_tw_name"]
+  );
 }
 
 /**
@@ -65,107 +66,106 @@ async function getRemoteStringsJSON_tw(config) {
  */
 async function getRemoteStringsJSON_en(config) {
   return await getRemoteStringsJSON_by_language(
-    config, config['remote_strings_en_name'])
+    config,
+    config["remote_strings_en_name"]
+  );
 }
 
+async function getGitkrakenVersion() {
+  return await new Promise(function(resolve, reject) {
+    child_process.exec("gitkraken -v", (error, stdout) => {
+      let gitkraken_version = String(stdout).trim();
+      resolve(gitkraken_version);
+    });
+  });
+}
 
 /**
- * 尋找並取代本地的 strings.json
- * (我知道寫的很醜，但是我也不想再改了
- * (我不會非同步啦 _(´ཀ`」 ∠)_
- * (請求支援
- * @param {string}} newStringsJSON 已經合併過的新的 JSON 語言檔
+ * @function replace_local_strings
+ * @description 替換 GitKraken 的語言檔
+ * @access public
+ *
+ * @param {string} newStringsJSON   已翻譯過的語言檔字串
+ * @param {string} gitkrakenVersion 這台電腦的 GitKraken 版本
+ *
+ * @return {void} 沒回傳只能看log
  */
-async function replace_local_strings(newStringsJSON) {
+async function replace_local_strings(newStringsJSON, gitkrakenVersion) {
   // 判斷不同平台並設立正確的 strings_path
-  const platform_type = os.type()
-  let strings_path
-  console.log(`platform_type is ${platform_type}`);
-  if (platform_type == 'Linux') {
-    // 在 Linux 上
-    strings_path = "/usr/share/gitkraken/resources/app.asar.unpacked/src"
-    fs.access(strings_path, fs.constants.F_OK, (err) => {
-      console.log(`${strings_path} ${err ? '不存在' : '存在'}`);
+  const platform_type = os.type();
+  let strings_path;
+  console.log(`Platform type is ${platform_type}`);
+
+  /**
+   * @function writeStringsFile
+   * @description 給 replace_local_strings 用來取代 local_strings
+   * @access private
+   *
+   * @param {string} strings_path 要取代的 strings 位置
+   *
+   * @return {void} 沒回傳只能看log
+   */
+  function writeStringsFile(strings_path) {
+    fs.access(strings_path, fs.constants.F_OK, err => {
+      console.log(`${strings_path} ${err ? "不存在" : "存在"}`);
       if (err) {
         // 不正確噴錯
-        console.error(err)
+        console.error(err);
       } else {
         // 取代 loacl 的語言檔
-        fs.writeFile('./strings.json', newStringsJSON, 'utf8', () => {
-          console.log('finished')
-        })
+        fs.writeFile("./strings.json", newStringsJSON, "utf8", () => {
+          console.log("取代完成！");
+          console.log("輸入任意鍵結束...");
+        });
       }
     });
-  } else if (platform_type == 'Darwin') {
+  }
+
+  // 生出 strings_path
+  if (platform_type == "Linux") {
+    // 在 Linux 上
+    strings_path = "/usr/share/gitkraken/resources/app.asar.unpacked/src";
+  } else if (platform_type == "Darwin") {
     // 在 Darwin(macOS) 上
     strings_path = [
       "/Applications/GitKraken.app/Contents/",
       "Resources/app.asar.unpacked/src/strings.json"
-    ].join('')
-    fs.access(strings_path, fs.constants.F_OK, (err) => {
-      console.log(`${strings_path} ${err ? '不存在' : '存在'}`);
-      if (err) {
-        // 不正確噴錯
-        logger.error(err)
-      } else {
-        // 取代 loacl 的語言檔
-        fs.writeFile('./strings.json', newStringsJSON, 'utf8', () => {
-          console.log('finished')
-        })
-      }
-    });
+    ].join("");
   } else {
     // 在 Windows_NT 上
-    // 生出 strings_path
-    await child_process.exec('gitkraken -v', (error, stdout) => {
-      let gitkraken_version = stdout
-      strings_path = [
-        process.env['LOCALAPPDATA'],
-        "\\gitkraken\\app-",
-        gitkraken_version,
-        `\\resources\\app.asar.unpacked\\src\\strings.json`,
-      ].join('').replace('\n', '')
-      console.log(`strings_path:\n${strings_path}`);
-
-      // 檢查生出來的 path 是否正確
-      fs.access(strings_path, fs.constants.F_OK, (err) => {
-        console.log(`${strings_path} ${err ? '不存在' : '存在'}`);
-        if (err) {
-          // 不正確噴錯
-          console.error(err)
-        } else {
-          // 取代 loacl 的語言檔
-          fs.writeFile(strings_path, newStringsJSON, 'utf8', (err) => {
-            if (err) throw err;
-            console.log('轉換 GitKraken 語言完成.')
-          })
-        }
-      });
-    })
+    strings_path = [
+      process.env["LOCALAPPDATA"],
+      "\\gitkraken\\app-",
+      gitkrakenVersion,
+      `\\resources\\app.asar.unpacked\\src\\strings.json`
+    ]
+      .join("")
+      .trim();
   }
-  console.log('輸入任意鍵結束.');
+  writeStringsFile(strings_path);
+
   if (process.stdin.isTTY) {
     process.stdin.setRawMode(true);
   }
   process.stdin.resume();
-  process.stdin.on('data', process.exit.bind(process, 0));
+  process.stdin.on("data", process.exit.bind(process, 0));
 }
 
-/**
- * 就...主程式(?
- */
 async function main() {
   // 抓遠端語言 JSON 下來
   let values = await Promise.all([
     getRemoteStringsJSON_en(config),
-    getRemoteStringsJSON_tw(config)
+    getRemoteStringsJSON_tw(config),
+    getGitkrakenVersion()
   ]);
-  const enJSON = values[0]
-  const twJSON = values[1]
+  const enJSON = values[0];
+  const twJSON = values[1];
+  const gitkrakenVersion = values[2];
+  console.log(`Loacl GitKraken version = ${gitkrakenVersion}`);
 
   // 進行取代後輸出新的 JSON 語言檔
-  const newStringsJSON = merge(enJSON, twJSON)
-  replace_local_strings(newStringsJSON)
+  const newStringsJSON = merge(enJSON, twJSON);
+  replace_local_strings(newStringsJSON, gitkrakenVersion);
 }
 
-main() // 施展魔法 (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+main(); // 施展魔法 (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
