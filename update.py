169f47500a5ce441cc1e9223be35c89f51a647bb
Mango Jelly Pudding <74277414+EvanHsieh0415@users.noreply.github.com>
2022-09-19 09:29:42 +0800

    Create update.py
    新增python3版的update.js

---
+++ update.py
@@ -0,0 +1,12 @@
+"""
+Soruce From EvanHsieh0415
+https://github.com/EvanHsieh0415
+"""
+
+from json import load, dump
+
+with open('./strings.en.json') as enJSON, open('./strings.json', encoding='utf8') as zhJSON:
+    data = load(enJSON) | load(zhJSON)
+
+with open('./strings.json', 'w', encoding='utf8') as JSON:
+    dump(data, JSON, indent=4, ensure_ascii=False)
