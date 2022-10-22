804036ed4fbf4f5e6bd32ad45bbf4a9644744b8e
Mango Jelly Pudding <74277414+EvanHsieh0415@users.noreply.github.com>
2022-10-23 00:03:16 +0800

    Update update.py

--- update.py
+++ update.py
@@ -5,7 +5,7 @@ https://github.com/EvanHsieh0415
 
 from json import load, dump
 
-with open('./strings.en.json') as enJSON, open('./strings.json', encoding='utf8') as zhJSON:
+with open('./strings.en.json', encoding='utf8') as enJSON, open('./strings.json', encoding='utf8') as zhJSON:
     data = load(enJSON) | load(zhJSON)
 
 with open('./strings.json', 'w', encoding='utf8') as JSON:
